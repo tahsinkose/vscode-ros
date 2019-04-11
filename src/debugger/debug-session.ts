b6395391191c1a9a00274ead5eeedab012dc7b0f
Tahsincan Kose <tahsinkose@yandex.com>
2019-04-11 17:10:49 +0300

    Revert "Revert "Start implementing debug-adapter""
    This reverts commit 7fbc15b1df33f0b7319767e7fa06bc7194d65760.

--- src/debugger/debug-session.ts
+++ src/debugger/debug-session.ts
@@ -1,10 +1,11 @@
 import * as cp from "child_process";
 import * as adapter from "vscode-debugadapter";
 import * as pfs from "../promise-fs";
-import { DebugProtocol as Protocol } from "vscode-debugprotocol";
+import { DebugProtocol } from "vscode-debugprotocol";
+import { EventEmitter } from "events";
+const { Subject } = require('await-notify');
 
-
-interface ILaunchRequestArguments extends Protocol.LaunchRequestArguments {
+interface ILaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
   command: "roslaunch" | "rosrun";
   package: string;
   target: string;
@@ -15,15 +16,62 @@ interface ILaunchRequestArguments extends Protocol.LaunchRequestArguments {
 export default class DebugSession extends adapter.DebugSession {
   private process: cp.ChildProcess;
   private static THREAD_ID = 1;
-  public shutdown() {
-    if (this.process) {
-      this.process.kill();
-    }
+  private _variableHandles = new adapter.Handles<string>();
+  private _configurationDone = new Subject();
 
+  public constructor()
+  {
+      super();
+      this.setDebuggerLinesStartAt1(false);
+      this.setDebuggerColumnsStartAt1(false);
+      this.on('stopOnEntry', () => {
+        this.sendEvent(new adapter.StoppedEvent('entry', DebugSession.THREAD_ID));
+      });
+      this.on('stopOnStep', () => {
+        this.sendEvent(new adapter.StoppedEvent('step', DebugSession.THREAD_ID));
+      });
+      this.on('stopOnBreakpoint', () => {
+        this.sendEvent(new adapter.StoppedEvent('breakpoint', DebugSession.THREAD_ID));
+      });
+      this.on('stopOnException', () => {
+        this.sendEvent(new adapter.StoppedEvent('exception', DebugSession.THREAD_ID));
+      });
+      this.on('breakpointValidated', (bp: DebugProtocol.Breakpoint) => {
+        this.sendEvent(new adapter.BreakpointEvent('changed', <DebugProtocol.Breakpoint>{ verified: bp.verified, id: bp.id }));
+      });
+      this.process.on("exit", () => this.sendEvent(new adapter.TerminatedEvent()));
+  }
+
+  /**
+	 * The 'initialize' request is the first request called by the frontend
+	 * to interrogate the features the debug adapter provides.
+	 */
+	protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+
+		// build and return the capabilities of this debug adapter:
+		response.body = response.body || {};
+
+		// the adapter implements the configurationDoneRequest.
+		response.body.supportsConfigurationDoneRequest = true;
+
+		// make VS Code to use 'evaluate' when hovering over source
+		response.body.supportsEvaluateForHovers = true;
+
+		// make VS Code to show a 'step back' button
+		response.body.supportsStepBack = true;
+
+		this.sendResponse(response);
+
+		// since this debug adapter can accept configuration requests like 'setBreakpoint' at any time,
+		// we request them early by sending an 'initializeRequest' to the frontend.
+		// The frontend will end the configuration sequence by calling 'configurationDone' request.
+		this.sendEvent(new adapter.InitializedEvent());
+}
+  public shutdown() {
     super.shutdown();
   }
 
-  protected launchRequest(response: Protocol.LaunchResponse, request: ILaunchRequestArguments) {
+  protected async launchRequest(response: DebugProtocol.LaunchResponse, request: ILaunchRequestArguments) {
     if (request.command !== "roslaunch" && request.command !== "rosrun") {
       this.sendErrorResponse(response, 0, "Invalid command");
       return;
@@ -32,23 +80,15 @@ export default class DebugSession extends adapter.DebugSession {
     // Merge the ROS env with the current env so we aren't running in headless mode.
     const args = [request.package, request.target].concat(request.args || []);
     const workspaceSetup = '/home/tahsin/dji_ws/devel/setup.bash';
-    const env = sourceSetupFile(workspaceSetup, {});
+    let env = await sourceSetupFile(workspaceSetup, {});
+    for(var key in process.env){
+      if(typeof env[key]=== 'undefined')
+      {
+        env[key] = process.env[key]
+      }
+    }
     this.process = cp.spawn(request.command, args, { env });
-    console.log(env)
-    this.process.stdout.on("data", chunk =>
-      this.sendEvent(new adapter.OutputEvent(chunk.toString(), "stdout"))
-    );
-    this.process.stderr.on("data", chunk =>
-      this.sendEvent(new adapter.OutputEvent(chunk.toString(), "stderr"))
-    );
-    this.process.on('stopOnBreakpoint', () => {
-			this.sendEvent(new adapter.StoppedEvent('breakpoint', DebugSession.THREAD_ID));
-    });
-    this.process.on("error", (err: Error) => {
-      this.sendEvent(new adapter.OutputEvent(err.message, "stderr"));
-      this.sendEvent(new adapter.TerminatedEvent());
-    });
-    this.process.on("exit", () => this.sendEvent(new adapter.TerminatedEvent()));
+    
 
     this.sendResponse(response);
   }
